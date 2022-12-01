1f97f450c0a31bdd62994eea5826d67c83430ebf
stackinspector <stackinspector@126.com>
2022-12-01 15:57:51 +0800

    bin_struct_complex_impl

--- lib.rs
+++ lib.rs
@@ -41,6 +41,77 @@ macro_rules! bin_struct {
 }
 
 #[macro_export]
+macro_rules! bin_struct_complex_impl {
+    {
+        $struct_name:ident { $($field:ident -> $_ftyk:tt($_ftyv:tt))* }
+    } => {
+        macro_rules! size {
+            (Number($ty:ty)) => {
+                std::mem::size_of::<$ty>()
+            };
+            (Bytes($size:literal)) => {
+                $size
+            };
+            (Struct($ty:ty)) => {
+                <$ty>::SIZE
+            };
+        }
+
+        impl $struct_name {
+            pub const SIZE: usize = $(size!($_ftyk($_ftyv))+)*0;
+
+            pub fn from_bytes(raw: [u8; Self::SIZE]) -> Self {
+                let mut offset: usize = 0;
+                $(let $field = {
+                    let _size = size!($_ftyk($_ftyv));
+                    let _slice = raw[offset..(offset + _size)].try_into().unwrap();
+                    macro_rules! from {
+                        (Number($ty:ty)) => {
+                            <$ty>::from_be_bytes(_slice)
+                        };
+                        (Bytes($size:literal)) => {
+                            _slice
+                        };
+                        (Struct($ty:ty)) => {
+                            <$ty>::from_bytes(_slice)
+                        };
+                    }
+                    let _val = from!($_ftyk($_ftyv));
+                    offset += _size;
+                    _val
+                };)*
+                assert_eq!(offset, Self::SIZE);
+                Self { $($field,)* }
+            }
+
+            pub fn to_bytes(&self) -> [u8; Self::SIZE] {
+                let mut buf = [0u8; Self::SIZE];
+                let mut offset: usize = 0;
+                $({
+                    let _size = size!($_ftyk($_ftyv));
+                    macro_rules! to {
+                        (Number($ty:ty)) => {
+                            self.$field.to_be_bytes()
+                        };
+                        (Bytes($size:literal)) => {
+                            self.$field
+                        };
+                        (Struct($ty:ty)) => {
+                            self.$field.to_bytes()
+                        };
+                    }
+                    let _slice = to!($_ftyk($_ftyv));
+                    (&mut buf[offset..(offset + _size)]).copy_from_slice(&_slice);
+                    offset += _size;
+                })*
+                assert_eq!(offset, Self::SIZE);
+                buf
+            }
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! error_enum {
     {
         #[derive($($derives:tt)*)]
